c0031a22e9f34becffda1c09607274bba9f30f8a
Akanimoh Osutuk <akan@Akanimohs-MacBook-Pro.local>
2023-01-01 23:27:44 +0100

    functions done

--- src/lectures/demo/functions/functions.go
+++ src/lectures/demo/functions/functions.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+func double(num int) int {
+	return num + num
+}
+
+func add(lhs, rhs int) int {
+	return lhs + rhs
+}
+
+func greet() {
+	fmt.Println("Hello from greet function")
+}
+
 func main() {
+	greet()
+	dozen := double(6)
+	fmt.Println(dozen)
+
+	bakersDozen := add(dozen, 1)
+	fmt.Println(bakersDozen)
 
 }
--- src/lectures/exercise/functions/functions.go
+++ src/lectures/exercise/functions/functions.go
@@ -17,8 +17,50 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+//   - Write a function that accepts a person's name as a function
+//     parameter and displays a greeting to that person.
+func greet(name string) {
+	fmt.Printf("Hello, %s!\n", name)
+}
+
+//   - Write a function that returns any message, and call it from within
+//     fmt.Println()
+//   - Write a function to add 3 numbers together, supplied as arguments, and
+//     return the answer
+func add(a, b, c int) int {
+	return a + b + c
+}
+
+// * Write a function that returns any number
+func number() int {
+	return 5
+}
+
+// * Write a function that returns any two numbers
+func returnTwo() (int, int) {
+	first := 5
+	second := 6
+
+	return first, second
+}
+
+//* Add three numbers together using any combination of the existing functions.
+
+//  * Print the result
+//* Call every function at least once
 
 func main() {
+	greet("John")
+	add(1, 2, 3)
+	rand := number()
+	fmt.Println(rand)
+	returnTwo()
+
+	addThree := add(number(), number(), number())
+	fmt.Println(addThree)
 
 }
